217d8c7aea8ad441b56fdc487167ca4f56c32e41
Julian Espinel <julianespinel@gmail.com>
2016-05-12 14:24:23 -0500

    Get dates from the view.

--- zoom/app/scripts/main/main.controller.js
+++ zoom/app/scripts/main/main.controller.js
@@ -8,20 +8,44 @@
  * MainController of the zoomApp
  */
 zoomApp.controller('MainController', MainController);
-MainController.$inject = ['$scope'];
+MainController.$inject = ['$scope', 'moment'];
 
-function MainController($scope) {
+function MainController($scope, moment) {
 
-  var main = this;
+  var main = $scope;
+  var date = new moment();
+  main.form = {
+    whatDoYouNeed: '',
+    serviceLocation: '',
+    date: getFormatedDate(date), 
+    day: getWeekDayName(date),
+    time: getTimeWithTimeZone(date)
+  };
+
+  console.log(main.form);
 
   main.setDate = setDate;
   main.setTime = setTime;
 
-  function setDate(selectedDate) {
-    console.log('selectedDate: ' + selectedDate);
+  function getFormatedDate(date) {
+    return moment(date).format('D/M/YYYY');
   }
 
-  function setTime(selectedTime) {
+  function getWeekDayName(date) {
+    return moment(date).format('dddd');
+  }
+
+  function getTimeWithTimeZone(date) {
+    return moment(date).format('LT Z');
+  }
+
+  function setDate(selectedDate, oldDate) {
+    console.log('selectedDate: ' + selectedDate);
+    main.form.date = getFormatedDate(selectedDate);
+  }
+
+  function setTime(selectedTime, oldTime) {
     console.log('selectedTime: ' + selectedTime);
+    main.form.time = getTimeWithTimeZone(date);
   }
 };
