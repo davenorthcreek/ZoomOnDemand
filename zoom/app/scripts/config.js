39f34459a39fd9c00c99e378454e9234ba27f0f1
Julian Espinel <julianespinel@gmail.com>
2016-05-17 15:17:39 -0500

    Comment login to be able to access home.

--- zoom/app/scripts/config.js
+++ zoom/app/scripts/config.js
@@ -1,5 +1,5 @@
 
-
+/*
 (function () {
     'use strict';
 
@@ -9,7 +9,7 @@
 
    
 
-    /** @ngInject */
+    // @ngInject
     config.$inject = ['API_URL', '$authProvider', '$windowProvider', '$locationProvider'];
     function config(API_URL, $authProvider, $windowProvider, $locationProvider) {
         $locationProvider.html5Mode(true).hashPrefix('!');
@@ -51,7 +51,7 @@
     }
  
 })();
-
+*/
 
 
 
