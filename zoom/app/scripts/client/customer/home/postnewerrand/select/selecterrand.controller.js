5177126af7a70ed09aa9cb093e7e540afc47c3ec
Julian Espinel <julianespinel@talsip.com>
2016-05-20 15:30:37 -0500

    Styles Zoom finished

--- zoom/app/scripts/client/customer/home/postnewerrand/select/selecterrand.controller.js
+++ zoom/app/scripts/client/customer/home/postnewerrand/select/selecterrand.controller.js
@@ -9,7 +9,7 @@
     /** @ngInject */
     SelectErrandController.$inject = ['$state', '$scope'];
     function SelectErrandController($state, $scope) {
-        // var vm = this;        
-     
+        var vm = this;    
+        vm.names = ["john", "bill", "charlie", "robert", "alban", "oscar", "marie", "celine", "brad", "drew", "rebecca", "michel", "francis", "jean", "paul", "pierre", "nicolas", "alfred", "gerard", "louis", "albert", "edouard", "benoit", "guillaume", "nicolas", "joseph"];    
     }
 })();
