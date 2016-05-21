254da3e638ab0ebd0ec8bc68c713ec4b202a40e0
sachin kumar <sachin.kumar@widevision.co.in>
2016-05-21 14:26:47 +0530

    Commit(21-05-16 14:26)

--- zoom/app/scripts/client/customer/payment/payment.controller.js
+++ zoom/app/scripts/client/customer/payment/payment.controller.js
@@ -133,6 +133,7 @@
 
         // Stripe Response Handler
         $scope.stripeCallback = function (code, result) {
+          
             if (result.error) {
                 $window.alert('it failed! error: ' + result.error.message);
             } else {
