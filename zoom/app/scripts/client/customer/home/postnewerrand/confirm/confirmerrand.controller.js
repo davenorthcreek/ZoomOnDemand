fb8dd08dd956ec92bbc170b273368f47065330e0
henri <henrih1@zoho.com>
2016-06-28 18:53:45 +0800

    post errand validation

--- zoom/app/scripts/client/customer/home/postnewerrand/confirm/confirmerrand.controller.js
+++ zoom/app/scripts/client/customer/home/postnewerrand/confirm/confirmerrand.controller.js
@@ -12,6 +12,34 @@
       var vm = this;
 
       vm.submitErrand = function () {
+        if (!$rootScope.errand.type_id) {
+          toastr.warning('Please select type of errand');
+          $state.go('app.home.postnewerrand.select');
+          return;          
+        }
+        if (!$rootScope.errand.zoom_office_id) {
+          toastr.warning('Please select service location');
+          $state.go('app.home.postnewerrand.select');
+          return;                    
+        }
+        if ($rootScope.errand.type.name == 'Delivery') {
+          if (!$rootScope.errand.pick_up_address) {
+            toastr.warning('Please input pick up location');
+            $state.go('app.home.postnewerrand.details');
+            return;                      
+          }
+          if (!$rootScope.errand.address) {
+            toastr.warning('Please input drop off location');
+            $state.go('app.home.postnewerrand.details');
+            return;                      
+          }
+        } else {
+          if (!$rootScope.errand.address) {
+            toastr.warning('Please input errand location');
+            $state.go('app.home.postnewerrand.details');
+            return;                      
+          }          
+        }
         $scope.$broadcast('submitErrand');
       }
 
