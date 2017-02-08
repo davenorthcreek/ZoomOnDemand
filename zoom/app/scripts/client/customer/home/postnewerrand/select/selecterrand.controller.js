20b4c3d4bbdf9b39ba53dc81af6e5e0188445e20
fnumono <wangpramono@gmail.com>
2017-02-08 11:54:31 -0800

    managing signup/login for bclient and changing the form

--- zoom/app/scripts/client/customer/home/postnewerrand/select/selecterrand.controller.js
+++ zoom/app/scripts/client/customer/home/postnewerrand/select/selecterrand.controller.js
@@ -7,9 +7,135 @@
         .controller('SelectErrandController', SelectErrandController);
 
     /** @ngInject */
-    SelectErrandController.$inject = ['$rootScope', '$state', '$scope', '$http', 'API_URL', '$log', 'dateFilter', '$window'];
-    function SelectErrandController($rootScope, $state, $scope, $http, API_URL, $log, dateFilter, $window) {
+    SelectErrandController.$inject = ['$rootScope', '$state', '$scope', '$http', 'API_URL', '$log', 'dateFilter', '$window', 'Upload'];
+    function SelectErrandController($rootScope, $state, $scope, $http, API_URL, $log, dateFilter, $window, Upload) {
       var vm = this;
+
+      //************************************************
+      // errand details
+      //************************************************
+
+
+      vm.addresserror = false;
+      vm.detailserror = false;
+      if (!$rootScope.errand.funds) {
+          $rootScope.errand.funds = 0;
+      }
+      if (!$rootScope.errand.task_uploads) {
+          $rootScope.errand.task_uploads = {};
+      }
+      if (!$rootScope.errand.task_uploads['normal']) {
+          $rootScope.errand.task_uploads['normal'] = [];
+      }
+      if (!$rootScope.errand.task_uploads['funds']) {
+          $rootScope.errand.task_uploads['funds'] = [];
+      }
+
+      $rootScope.errand.pick_up_addr = "los angeles";
+
+      vm.autocompleteOptions = {
+          componentRestrictions: { country: 'us' },
+          types: ['geocode']
+      }
+
+       vm.blurAddress = function () {
+          if (($rootScope.errand.addr) && ($rootScope.errand.addr.types)) {
+              var p = $rootScope.errand.addr;
+              for (var i = 0; i < p.address_components.length; i++) {
+                  var addressType = p.address_components[i].types[0];
+                  if (addressType == "locality") {
+                      $rootScope.errand.city = p.address_components[i]['long_name'];
+                      break;
+                  }
+              }
+              if (!$rootScope.errand.city) {
+                  vm.invalidAddress = true;
+                  return;
+              }
+
+              $rootScope.errand.address = $rootScope.errand.addr.formatted_address;
+              $rootScope.errand.addrlat = $rootScope.errand.addr.geometry.location.lat();
+              $rootScope.errand.addrlng = $rootScope.errand.addr.geometry.location.lng();
+              vm.invalidAddress = false;
+          } else {
+              vm.invalidAddress = true;
+          }
+      }
+
+      vm.blurPickUpAddress = function () {
+          var city;
+          if (($rootScope.errand.pick_up_addr) && ($rootScope.errand.pick_up_addr.types)) {
+              var p = $rootScope.errand.pick_up_addr;
+              for (var i = 0; i < p.address_components.length; i++) {
+                  var addressType = p.address_components[i].types[0];
+                  if (addressType == "locality") {
+                      city = p.address_components[i]['long_name'];
+                      break;
+                  }
+              }
+              if (!city) {
+                  vm.invalidPickUpAddress = true;
+                  return;
+              }
+
+              $rootScope.errand.pick_up_address = $rootScope.errand.pick_up_addr.formatted_address;
+              $rootScope.errand.pick_up_addrlat = $rootScope.errand.pick_up_addr.geometry.location.lat();
+              $rootScope.errand.pick_up_addrlng = $rootScope.errand.pick_up_addr.geometry.location.lng();
+              vm.invalidPickUpAddress = false;
+          } else {
+              vm.invalidPickUpAddress = true;
+          }
+      }
+
+      vm.uploadFile = function (files, category) {
+          if (files && files.length) {
+              Upload.upload({
+                  url: API_URL + '/bclient/tasks/upload_files',
+                  data: { file: files, category: category }
+              }).then(function (resp) {
+                  console.log('Success uploaded. Response: ' + resp.data);
+                  $rootScope.errand.task_uploads[category] = $rootScope.errand.task_uploads[category].concat(resp.data)
+              }, function (resp) {
+                  console.log('Error status: ' + resp.status);
+              }, function (evt) {
+                  var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+                  console.log('progress: ' + progressPercentage + '% ');
+              });
+          }
+      }
+
+        $rootScope.$watch('errand.address', function () {
+            if ($rootScope.errand.address == undefined) {
+                //  vm.addresserror = true;
+            } else {
+                vm.addresserror = false;
+            }
+        }, true);
+        $rootScope.$watch('errand.pick_up_address', function () {
+            if ($rootScope.errand.pick_up_address == undefined) {
+                //  vm.addresserror = true;
+            } else {
+                vm.pick_up_address_error = false;
+            }
+        }, true);        
+        $rootScope.$watch('errand.details', function () {
+            if ($rootScope.errand.details == undefined) {
+                // vm.detailserror = true;
+            } else {
+                vm.detailserror = false;
+            }
+        }, true);
+        $rootScope.$watch('errand.item', function () {
+            if ($rootScope.errand.item == undefined) {
+                // vm.detailserror = true;
+            } else {
+                vm.item_error = false;
+            }
+        }, true);
+
+      //*******************************************************
+      //select errand
+      //*******************************************************
 
       vm.locations = ['Los Angeles', 'San Diego'];
       vm.frequencies = [];
@@ -66,7 +192,7 @@
           $log.log(vm.all_types);
         });
 
-      $http.get(API_URL + '/client/zoomoffices')
+      $http.get(API_URL + '/bclient/zoomoffices')
         .then(function(resp) {
           vm.zoomoffices = resp.data;
           $log.log(vm.zoomoffices);
@@ -105,15 +231,34 @@
               vm.datetimeerrorShow = true;
               count++;
           }
-          if($rootScope.errand.type_id == undefined)
+          //***************************************************
+          //errand details
+          //***************************************************
+
+          console.log("$rootScope.errand", $rootScope.errand)
+          vm.addresserror = false;
+          vm.detailserror = false;
+
+          if ($rootScope.errand.address == undefined)
           {
-              vm.type_iderror = true;
-              count++;
+            count++;
+            vm.addresserror = true;
           }
+
+          if ($rootScope.errand.details == undefined ||$rootScope.errand.details == "")
+          {
+            count++;
+            vm.detailserror = true
+          }
+
           if (count == 0)
           {
               $state.go('app.home.postnewerrand.details');
           }
+
+          // if (count == 0) {
+          //     $state.go('app.home.postnewerrand.confirm.usehours');
+          //   }
 
       }
 
